25270a046f03b39b3125f961ca0c032f2dccf108
aqsafaisal34 <aqsay.faisal34@gmail.com>
2023-01-30 19:50:53 +0500

    work done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
+const getLocalItem = () => {
+  let list = localStorage.getItem("lists");
+  console.log(list);
+  if (list) {
+    return JSON.parse(list);
+  } else {
+    return [];
+  }
+};
 function App() {
+  const [text, setText] = useState("");
+  const [task, setTask] = useState(getLocalItem());
+
+  
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setTask([...task, text]);
+
+    setText("");
+  };
+  const removeTask = (a) => {
+    const finalData = task.filter((curEle, index) => {
+      return index !== a;
+    });
+
+    setTask(finalData);
+  };
+
+  useEffect(() => {
+    localStorage.setItem("lists", JSON.stringify(task));
+  }, [task]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <div className="row justify-content-center align-items-center main-row">
+          <div className="col shadow main-col bg-white">
+            <div className="row bg-primary text-white">
+              <div className="col  p-2">
+                <h4 className="text-center">Todo App Using React JS</h4>
+              </div>
+            </div>
+            <form onSubmit={submitHandler}>
+              <div className="row justify-content-between text-white p-2">
+                <div className="form-group flex-fill mb-2 col-9">
+                  <input
+                    id="todo-input"
+                    type="text"
+                    className="form-control"
+                    value={text}
+                    onChange={(e)=> setText(e.target.value)}
+                  />
+                </div>
+                <button
+                  type="submit"
+                  className="btn btn-primary mb-2 ml-2 col-3"
+                >
+                  Add todo
+                </button>
+              </div>
+            </form>
+
+            <div className="container">
+              <div className="row">
+                {task.map((value, index) => {
+                  return (
+                    <>
+                      <div className="col-6 my-2" key={index} id="list">
+                        {value}
+                      </div>
+                      <div className="col-6 my-2">
+                        <button id="dlt-btn" onClick={() => removeTask(index)}>X</button>
+                      </div>
+                    </>
+                  );
+                })}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
